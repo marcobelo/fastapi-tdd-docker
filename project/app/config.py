096695ab12fee0a7640ded0fd9229938ca7c6d97
Marco Belo <marco.barone.belo@gmail.com>
2022-06-22 22:08:54 -0300

    part-1: postgres setup

--- project/app/config.py
+++ project/app/config.py
@@ -2,7 +2,7 @@ from functools import lru_cache
 import logging
 import os
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 
 log = logging.getLogger("uvicorn")
 
@@ -10,7 +10,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = os.getenv("ENVIRONMENT", "dev")
     testing: bool = os.getenv("TESTING", 0)
-
+    database_url: AnyUrl = os.environ.get("DATABASE_URL")
 
 @lru_cache()
 def get_settings() -> BaseSettings:
